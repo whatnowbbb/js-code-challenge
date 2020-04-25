9c4e748513e7a42bae15288fd6c523b7f7601f30
whatnowbbb <63573601+whatnowbbb@users.noreply.github.com>
2020-04-25 11:43:46 +0800

    Update solution_b.js

--- src/three_ways_to_sum_to_n/solution_b.js
+++ src/three_ways_to_sum_to_n/solution_b.js
@@ -4,5 +4,9 @@
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
 var sum_to_n = function(n) {
-    // your code here
+    let arr = [1, 2, 3, 4, 5];
+    
+    n = arr.reduce((sum, current) => sum + current);
+    
+    alert(n);
 };
